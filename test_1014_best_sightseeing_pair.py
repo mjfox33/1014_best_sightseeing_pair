9794e238d2d61d2abd5f037acb71630ab6463e2f
mjfox33 <mjfox33@gmail.com>
2022-02-08 20:10:19 -0500

    initial commit - timing out w a large input array

---
+++ test_1014_best_sightseeing_pair.py
@@ -0,0 +1,9 @@
+import code_1014_best_sightseeing_pair as c
+
+def test_example_1():
+    s = c.Solution()
+    assert s.maxScoreSightseeingPair([8,1,5,2,6]) == 11
+
+def test_example_2():
+    s = c.Solution()
+    assert s.maxScoreSightseeingPair([1,2]) == 2
